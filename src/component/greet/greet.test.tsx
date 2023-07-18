8f169715538f2f7baa1147e96f43391f0cfa3326
Samson Tmg <stmg202023@gmail.com>
2023-07-18 12:07:31 +0545

    multiple test group test in jest

--- src/component/greet/greet.test.tsx
+++ src/component/greet/greet.test.tsx
@@ -1,3 +1,4 @@
+//MULTIPLE GROUP IN JEST TEST
 // Greet should render text hello and if a name is passed into the Greet component then
 // it should render the name text too
 // test.only() and test.skip()
@@ -9,26 +10,25 @@ import Greet from "./greet";
 //THIS IS EXAMPLE OF GROUP TESTS IN JEST
 describe("Greet", () => {
   //
-  test("Greet should render hello", () => {
+  test("should render hello", () => {
     render(<Greet />);
 
     const textElement = screen.getByText("Hello");
 
     expect(textElement).toBeInTheDocument();
   });
+});
 
-  //THIS IS NESTED
-  describe("Nested", () => {
-    test("should render a name props", () => {
-      const name = "Samson";
+describe("Nested", () => {
+  test("should render a name props", () => {
+    const name = "Samson";
 
-      render(<Greet name={name} />);
+    render(<Greet name={name} />);
 
-      // const textElement = screen.getByText(new RegExp(name, "i")); // Use RegExp with "i" flag for case-insensitive matching
-      const textElement = screen.getByText("Hello Samson");
+    // const textElement = screen.getByText(new RegExp(name, "i")); // Use RegExp with "i" flag for case-insensitive matching
+    const textElement = screen.getByText("Hello Samson");
 
-      expect(textElement).toBeInTheDocument();
-    });
-    //
+    expect(textElement).toBeInTheDocument();
   });
+  //
 });
