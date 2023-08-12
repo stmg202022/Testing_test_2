63df37786dc37ecfbffab327b971231414c11a1a
Samson Tmg <stmg202023@gmail.com>
2023-08-12 22:01:44 +0545

    act() utility // used for updating Component state // used for async functions (data-fetching)

--- src/Hooks/use_counter/useCounter.test.tsx
+++ src/Hooks/use_counter/useCounter.test.tsx
@@ -1,13 +1,11 @@
 // import { render } from "@testing-library/react";
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { useCounter } from "./useCounter";
 
 describe("useCounter", () => {
   test("should render an initial count value correctly ", () => {
     // render(<useCounter />); //it is giving a error due to dataTypes and declearing hook at outer fn
-
     const { result } = renderHook(useCounter); //get the result return by useCounter Hook
-
     expect(result.current.count).toBe(0);
   });
 
@@ -17,7 +15,18 @@ describe("useCounter", () => {
         initialCount: 10,
       },
     });
-
     expect(result.current.count).toBe(10);
+  });
+
+  test("should increment the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+
+  test("should decrement the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
   });
 });
